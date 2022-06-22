21a301ce4acacb769d725c46c7aca3601bf8e0e0
Julian Murillo <jdawg.chapulin@gmail.com>
2022-06-22 08:51:34 -0700

    first commit

---
+++ javascript/script.js
@@ -0,0 +1,10 @@
+function gaming(){
+    document.getElementById('videogames').src='https://cdn.vox-cdn.com/thumbor/VX2HYaCjG6rXqBj3VZmdWb1gEz0=/0x0:2040x1360/1200x800/filters:focal(638x515:964x841)/cdn.vox-cdn.com/uploads/chorus_image/image/65967729/akrales_190201_3192_0240.0.jpg';
+}
+function jujitsu(){
+    document.getElementById('videogames').src='https://upload.wikimedia.org/wikipedia/commons/1/14/JUJITSU_%28AND_RIFLES%29_in_an_agricultural_school.jpg';
+}    
+function comics(){
+    document.getElementById('videogames').src='https://s2982.pcdn.co/wp-content/uploads/2021/04/free-comics-online.png';
+}    
+
